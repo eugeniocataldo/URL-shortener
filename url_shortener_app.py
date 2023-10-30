a4755815a788958641887e8b3765a91f48b10eae
Eugenio Cataldo <eugenio.cataldo@gmail.com>
2023-10-30 21:09:13 +0100

    Implement generate_short_url function

--- url_shortener_app.py
+++ url_shortener_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import pandas as pd
+import string, random
 
 app = Flask(__name__)
 
@@ -17,8 +18,18 @@ def rootpage():
 
     return render_template("shorten_url.html",
                            input_url=url)
+def generate_short_url():
 
+    while True:
+        characters = string.ascii_letters + string.digits
+        short_url = ''.join(random.choice(characters) for _ in range(6))
 
+        
+        #TODO: Add a check that the short_url is not in the database yet
+        url_existing = False
+        if not url_existing: 
+            return short_url
+        
 
 app.run()
 
